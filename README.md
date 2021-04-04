b992502419a7fbccf49cc0e344ddc0c834e0ab84
IlyaZ <zarubin@wiso.uni-koeln.de>
2021-04-04 18:37:04 +0200

    initial commit: blueprint of Leahs PMMH examples to be changed to SV model

---
+++ README.md
@@ -0,0 +1,35 @@
+# ## SVmodelExamples
+# 
+# This is an example of how to use RcppSMC in a package. I’m not sure whether this example is going to go
+# into the paper yet and it’s not fully documented, but it should give you some idea of how to create
+# another package that uses RcppSMC. It is fairly basic, using only a single function which is in C++ and no R functions.
+# 
+# At this stage, you will need to use the Github version of RcppSMC to be able to access RcppSMC.h. After
+# we update CRAN (soon), this won't be required.
+# 
+# ### How to build the package
+# You can build the package using
+# 
+# ```{r, eval = FALSE}
+# library('devtools')
+# install_github("rcppsmc/rcppsmc")
+# Rcpp::compileAttributes("SVmodelPMMH")
+# devtools::document("SVmodelPMMH") # if you have Roxygen documentation
+# devtools::load_all('SVmodelPMMH')
+# devtools::build('SVmodelPMMH')
+# ```
+# 
+# and look at the documentation with `?SVmodelPMMH`. There is some example code in the documentation, but you may wish to reduce `N1` and `N2` to 100 for a quick run.
+# 
+# ### Using RcppSMC without creating your own package
+# 
+# If you wanted to use RcppSMC without creating your own package, you can do so using \code{sourceCpp}.
+# 
+# 
+# Simply add the line `//[[Rcpp::depends(RcppSMC)]]` to the top of your C++ file, SMC_Ricker.cpp and replace `library(SVmodelPMMH)` in the example code from the documentation with
+# 
+# ```{r, eval = FALSE}
+# #devtools::install_github("rcppsmc/rcppsmc")
+# library(RcppSMC)
+# Rcpp::sourceCpp('SMC_Ricker.cpp')
+# ```
---
+++ src/SVmodelPMMH.h
@@ -0,0 +1,52 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+//
+// SVmodelPMMH.h: Example 3.1 of Andrieu et al. (2010). Implementing particle marginal
+// Metropolis-Hastings for a toy non-linear state space model previously described in
+// Gordon et al. (1993) and Kitagawa (1996).
+//
+// Copyright (C) 2017         Dirk Eddelbuettel, Adam Johansen and Leah South
+//
+// This file is part of RcppSMC.
+//
+// RcppSMC is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// RcppSMC is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with RcppSMC.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "RcppSMC.h"
+
+namespace SVmodelPMMH {
+
+    class parameters
+    {
+    public:
+        double sigv, sigw;
+    };
+    arma::vec y; //data
+
+    double logPrior(const parameters & proposal);
+
+    //A derived class for the moves
+    class SVmodelPMMH_move:
+    public smc::moveset<double,smc::nullParams>
+    {
+    public:
+
+        void pfInitialise(double & value, double & logweight, smc::nullParams & param);
+        void pfMove(long lTime, double & value, double & logweight, smc::nullParams & param);
+
+        ~SVmodelPMMH_move() {};
+
+    };
+
+    parameters theta_prop;
+	smc::moveset<double,smc::nullParams>* myMove;
+}
